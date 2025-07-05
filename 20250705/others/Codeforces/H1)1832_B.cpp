3d3b36dcf4bdff565b27e9f5151eec48623e9777
urbanetrex <urbanetrex@gmail.com>
2025-07-06 09:54:33 +1000

    third commit

--- 20250705/others/Codeforces/H1)1832_B.cpp
+++ 20250705/others/Codeforces/H1)1832_B.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
 
 using namespace std;
 
@@ -16,33 +15,17 @@ void solve() {
     sort(a.begin(), a.end());
 
     // calculate prefix sums
-    vector<lint> pfx(n);
-    pfx[0]=a[0];
-    for(int i=1; i<n; i++)pfx[i]=pfx[i-1]+a[i];
+    vector<lint> pfx(n+1);
+    pfx[0]=0;
+    for(int i=1; i<=n; i++)pfx[i]=pfx[i-1]+a[i-1];
 
-    lint min_deleted=LLONG_MAX;
+    lint maximum=0;
     for (int x = 0; x <= k; ++x) {
-        int removed_small = 2 * x;
-        int removed_large = k - x;
-    
-        if (removed_small + removed_large > n) continue;
-    
-        lint sum_small = (removed_small > 0) ? pfx[removed_small - 1] : 0;
-    
-        lint sum_large = pfx[n-1-removed_large] - pfx[n-1];
-
-
-
-        /*
-        for (int i = 0; i < removed_large; ++i)
-            sum_large += a[n - 1 - i]; // sum from a[n-1] to a[n-1-removed_large-1]
-        */
-
-        lint total_deleted = sum_small + sum_large;
-        min_deleted = min(min_deleted, total_deleted);
+        lint left = pfx[n+x-k] - pfx[x+x];
+        maximum = max(maximum, left);
     }
     
-    cout<<pfx[n-1]-min_deleted<<endl;
+    cout<<maximum<<endl;
 }
 
 int main() {
